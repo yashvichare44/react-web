02a5ace2d0244ff46cab5cdf3dd3b0ca686eadd4
yashvichare44 <yashvichare12@gmail.com>
2023-09-17 20:22:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from './Navbar';
+import './App.css';
+import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './Services';
+import Products from './Product';
+import SignUp from './SignUp';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
